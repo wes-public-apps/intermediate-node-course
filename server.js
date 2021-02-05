5ad17ab737514c6fbaed9ad7ecfa99f7e2f73113
wes-public-apps <murraywj@yahoo.com>
2021-02-05 13:00:57 -0800

    connect server to mongodb

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
+//import libraries
 const express= require('express');
 const mongoose= require('mongoose');
 const bodyParser= require('body-parser');
+const User = require('./models/User'); //import user schema
+
+//define constants
 const port=3000;
 const app= express();
 
+//register library functionality
 app.use(bodyParser.json());
 
+//list for traffic on port
 app.listen(port, ()=>{
 	console.log(`server is listening on port:${port}`)
 })
+
+//connect to database
+mongoose.connect('mongodb://localhost/userData');
 
 // CREATE
 app.post('/users',(req,res)=>{
